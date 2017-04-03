02572f75217f329db4a81a146e0469c474dc65b8
Yishan Wen <wittywys@gmaill.com>
2017-04-03 11:02:30 +0800

    add email template

---
+++ email/meta.js
@@ -0,0 +1,57 @@
+module.exports = {
+   subject: 'JavaScript Monthly #15',
+   issue_text: 'Issue #15  2nd Feb, 2017',
+   categories: [
+     {
+      name: 'Tech',
+      articles: [
+        {
+          title: 'React fix it',
+          link: 'https://github.com/MicheleBertoli/react-fix-it',
+          description: '黑科技--测试生成驱动React开发'
+        },
+        {
+          title: 'Zazu App',
+          link: 'http://zazuapp.org/documentation/',
+          description: 'Nodejs版本的Alfred，跨平台'
+        },
+        {
+          title: 'Gun',
+          link: 'https://github.com/amark/gun',
+          description: '轻量级Distributed DB'
+        }
+      ]
+     }, 
+     {
+      name: 'REPOS',
+      articles: [
+        {
+          title: 'Kap',
+          link: 'https://github.com/wulkano/kap',
+          description: '前端攻城狮还能怎么玩，写个录屏应用吧'
+        },
+        {
+          title: 'Labrador',
+          link: 'https://github.com/maichong/labrador',
+          description: '想高端地玩微信小程序，一定不能错过这个'
+        }
+      ]
+     },
+     {
+      name: 'BASIC',
+      articles: [
+        {
+          title: 'React Little Router',
+          link: 'https://github.com/FormidableLabs/redux-little-router',
+          description: '高性能轻量级react redux router方案，如果你的应用在变得臃肿，请考虑这个'
+        },
+        {
+          title: 'eleven-npm-tricks-that-will-knock-your-wombat-socks-off',
+          link: 'https://nodesource.com/blog/eleven-npm-tricks-that-will-knock-your-wombat-socks-off/?utm_source=nodeweekly&utm_medium=email',
+          description: '11个你不知道的NPM奇葩功能'
+        }
+      ]
+     }
+   ],
+   editor: ['文一山', '曾磊']
+}

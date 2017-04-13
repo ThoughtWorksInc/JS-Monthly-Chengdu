a210c708b5f80b6b38ef19c50fee592a05c86bd4
Yishan Wen <wittywys@gmail.com>
2017-04-14 03:20:04 +0800

    gen email step ok

--- email/parse.js
+++ email/parse.js
@@ -1,26 +1,41 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline')
+const readline = require('readline');
+const swig = require('swig');
+let argv = require('yargs').argv;
 
-const issue = fs.createReadStream(path.resolve(process.cwd(), './source/_posts/monthly-issue-15.md'), 'utf-8');
+let file = argv.f;
+let subject = argv.s;
+let issue_text = argv.i;
+let editors = argv.e;
 
+const issue = fs.createReadStream(path.resolve(process.cwd(), file), 'utf-8');
 
-const rl = readline.createInterface({
-  input: issue,
-  output: null
-})
+const meta = {
+  subject: subject,
+  issue_text: issue_text,
+  categories: [],
+  editors: editors && editors.split(',') || []
+};
 
-const meta = [];
 let category = {};
 let artile = {};
 
 let inCategory = false;
 let inArticle = false;
 
+const rl = readline.createInterface({
+  input: issue,
+  output: null
+})
+
+
 rl.on('line', function (line) {
   if (line.startsWith('####')) {
     if (inCategory) {
-      meta.push(category);
+      meta.categories.push(category);
       category = {};
     }
     inCategory = true;
@@ -28,16 +43,16 @@ rl.on('line', function (line) {
     category.articles = [];
     return;
   }
-  
+
   linkTextMatch = line.match(/(?:__|[*#])|\[(.*?)\]\((.*?)\)/);
-  
-  if(linkTextMatch) {    
+
+  if (linkTextMatch) {
     inArticle = true;
     artile.title = linkTextMatch[1];
     artile.link = linkTextMatch[2];
     return;
   }
-  
+
   if (inArticle) {
     artile.description = line;
     category.articles.push(artile);
@@ -48,8 +63,14 @@ rl.on('line', function (line) {
 });
 
 var util = require('util');
-rl.on('close', function() {
-  meta.push(category);
-  console.log(util.inspect(meta, {showHidden: false, depth: null}));
-})
+rl.on('close', function () {
+  meta.categories.push(category);
+  // console.log(util.inspect(meta, { showHidden: false, depth: null }));
 
+ const result = swig.renderFile(path.resolve(process.cwd(), './email/email.tpl.html'), meta);
+
+  fs.writeFile(path.resolve(process.cwd(), './email/email.html'), result, (err) => {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  })
+})
